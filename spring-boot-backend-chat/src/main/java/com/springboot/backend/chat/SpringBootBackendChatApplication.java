b62743551c49ac176fd9b4f325909046403b2750
Marcos Bertolotti <marcos.bertolotti@hotmail.com>
2020-03-05 20:59:12 -0300

    first commit

---
+++ spring-boot-backend-chat/src/main/java/com/springboot/backend/chat/SpringBootBackendChatApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.backend.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendChatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootBackendChatApplication.class, args);
+	}
+
+}
---
+++ spring-boot-backend-chat/src/test/java/com/springboot/backend/chat/SpringBootBackendChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.backend.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
